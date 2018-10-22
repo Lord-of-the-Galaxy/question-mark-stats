3ecb1be66f61496c1e508a52497fba017c4cb5cc
ed588 <calvin.588.edwards@gmail.com>
2018-10-22 20:58:32 +0100

    initial commit

---
+++ bot.py
@@ -0,0 +1,72 @@
+import discord
+from discord.ext import commands
+import logging
+import matplotlib.pyplot as plt
+from io import BytesIO
+from conf import channel_ids, initial_message
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s [%(name)s] %(message)s",
+    datefmt="%H:%M:%S"
+)
+logger = logging.getLogger("question")
+
+bot = commands.Bot("!!")
+
+@bot.event
+async def on_ready():
+    logger.info("Logged in as {0.user}".format(bot))
+
+@bot.event
+async def on_command_error(ctx,err):
+	await ctx.send(err)
+
+@commands.guild_only()
+@commands.cooldown(1, 60)
+@bot.command()
+async def go(ctx):
+	ch = bot.get_channel(channel_ids['monitor'])
+	ch_rep = bot.get_channel(channel_ids['report'])
+	start = await ch.get_message(initial_message)
+	count = 0
+	by_users = {}
+	async for msg in ch.history(limit=None, after=start):
+		if msg.content != "?" and msg.type == discord.MessageType.default:
+			await report_bad(msg, ch_rep)
+		else:
+			count += 1
+			if msg.author.name not in by_users:
+				by_users[msg.author.name] = 1
+			else:
+				by_users[msg.author.name] += 1
+	await ch_rep.send("I found {} messages that are question marks...".format(count))
+	await ch_rep.send("Pie chart coming up...")
+	with ch_rep.typing():
+		by_users = {k:v for k,v in by_users.items() if v > (count/100)}
+		labels, values = zip(*sorted(by_users.items(), key=lambda kv: kv[1], reverse=True))
+		nowtotal = sum(values)
+		labels=list(labels)
+		values=list(values)
+		labels.append("Other")
+		values.append(count-nowtotal)
+		fig, ax = plt.subplots()
+		ax.pie(values, labels=labels, autopct=lambda p: '{:.0f}'.format(p * sum(values) / 70), startangle=90)
+		ax.axis("equal")
+		buff = BytesIO()
+		fig.savefig(buff, format="png")
+		buff.seek(0)
+		f = discord.File(buff, "pie.png")
+		await ch_rep.send(file=f)
+		buff.close()
+
+async def report_bad(msg, ch_rep):
+    lstr = "I found a message saying [{0.content}] (by {0.author}, on {0.created_at})".format(msg)
+    await ch_rep.send(lstr)
+    try:
+        await msg.pin()
+    except discord.Forbidden:
+        await ch_rep.send("I couldn't pin a message!")
+
+token = open("token", "r").read().rstrip()
+bot.run(token)
---
+++ conf.py
@@ -0,0 +1,6 @@
+channel_ids = {
+    "monitor": 403641228505120775,
+    "report": 436328033817985025
+}
+
+initial_message = 433653512057126912
